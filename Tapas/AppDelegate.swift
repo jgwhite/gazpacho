617a6fddda6e2f4ae57bdcbe4039a2bc6b4a2f44
Jamie White <jamie@jgwhite.co.uk>
2015-06-14 16:30:07 +0100

    Initial Commit

---
+++ Tapas/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tapas
+//
+//  Created by Jamie White on 14/06/2015.
+//  Copyright © 2015 Jamie White. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
