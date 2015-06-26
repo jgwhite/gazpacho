06c9c71ff52d84be578fbfc9219d9cd41bbdc25b
Jamie White <jamie@jgwhite.co.uk>
2015-06-26 15:17:13 +0100

    Make main window behave properly

--- Tapas/AppDelegate.swift
+++ Tapas/AppDelegate.swift
@@ -11,16 +11,26 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    @IBAction func showMainWindow(sender: AnyObject) {
+        showMainWindow()
+    }
 
+    func showMainWindow() {
+        mainWindow?.makeKeyAndOrderFront(self)
+    }
+
+    var mainWindow: NSWindow? {
+        return NSApplication.sharedApplication().windows[0]
+    }
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        mainWindow?.excludedFromWindowsMenu = true
     }
 
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+    func applicationShouldHandleReopen(app: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        showMainWindow()
+        return true
     }
-
 
 }
 
